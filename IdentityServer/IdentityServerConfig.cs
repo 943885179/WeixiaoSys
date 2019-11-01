6e5f2a3ff764b30abd504958bf29a0de1b62fee8
mengzujun <943885179@qq.com>
2019-11-01 16:49:20 +0800

    框架提交

---
+++ IdentityServer/IdentityServerConfig.cs
@@ -0,0 +1,108 @@
+﻿using IdentityServer4;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class IdentityServerConfig
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new IdentityResource[] {
+                new IdentityResources.OpenId()
+            };
+        }
+        public static IEnumerable<ApiResource> GetApis()
+        {
+            return new List<ApiResource>()
+            {
+                new ApiResource("app","myapp1")
+            };
+        }
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>() {//使用connect/token调用
+                new Client
+                    {
+                        ClientId = "client",
+                        // 没有交互性用户，使用 clientid/secret 实现认证。
+                        AllowedGrantTypes = GrantTypes.ClientCredentials,//设置模式，客户端模式
+                        //用于认证的密码
+                        ClientSecrets =
+                        {
+                            new Secret("secret".Sha256())
+                        },
+                        // scopes that client has access to
+                        //客户端有权访问的范围（Scopes）
+                        AllowedScopes = { "app" }
+                    },
+                new Client()
+                {
+                    ClientId="PasswordClient",
+                    AllowedGrantTypes=GrantTypes.ResourceOwnerPassword,
+                    ClientSecrets={ new Secret("secretPassword".Sha256())},
+                    AllowedScopes={ "app" }
+                },
+                 new Client()
+                {
+                    ClientId="ImpLicitClient",
+                    AllowedGrantTypes=GrantTypes.Implicit,//OpenID Connect 简化模式（implicit）
+                    ClientSecrets={ new Secret("implicitSecrets".Sha256()) },
+                    RequireConsent=true,   //用户选择同意认证授权
+                    RedirectUris={ "https://localhost:5008/signin-oidc" }, //指定允许的URI返回令牌或授权码(我们的客户端地址)
+                    PostLogoutRedirectUris={ "https://localhost:5008/signout-callback-oidc" },//注销后重定向地址 
+                    LogoUri="https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=3298365745,618961144&fm=27&gp=0.jpg",
+                  ////运行访问的资源
+                     AllowedScopes = {                       //客户端允许访问个人信息资源的范围
+                      IdentityServerConstants.StandardScopes.Profile,
+                      IdentityServerConstants.StandardScopes.OpenId,
+                      IdentityServerConstants.StandardScopes.Email,
+                      IdentityServerConstants.StandardScopes.Address,
+                      IdentityServerConstants.StandardScopes.Phone,
+                      "app"
+                     }
+                 },new Client()
+                 {
+                     ClientId = "mvc",
+                     ClientName = "Hybrid",
+                     AllowedGrantTypes = GrantTypes.Hybrid,
+                     ClientSecrets =
+                     {
+                         new Secret("secret".Sha256())
+                     },
+                     RedirectUris           = { "http://localhost:5002/signin-oidc" },
+                     PostLogoutRedirectUris = { "http://localhost:5002/signout-callback-oidc" },
+                     AllowedScopes =
+                     {
+                         IdentityServerConstants.StandardScopes.OpenId,
+                         IdentityServerConstants.StandardScopes.Profile,
+                         "app"
+   
+                     },
+                     AllowOfflineAccess = true
+                 }
+
+            };
+        }
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>() {
+                new TestUser(){
+                 SubjectId="001",
+                 Username="weixiao",
+                 Password="123"
+                }
+            };
+        }
+      
+    }
+}
---
+++ IdentityServer/Startup.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace IdentityServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var op = Configuration.GetConnectionString("Default");
+            // services.AddDbContext<MyDbContext>(options => options.UseMySql(op));
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+            services.AddControllers();
+            services.AddIdentityServer()
+                .AddInMemoryIdentityResources(IdentityServerConfig.GetIdentityResources())
+                .AddInMemoryApiResources(IdentityServerConfig.GetApis())
+                .AddInMemoryClients(IdentityServerConfig.GetClients())
+                //.AddTestUsers(IdentityServerConfig.GetUsers())
+                .AddConfigurationStore(options => options.ConfigureDbContext = b => b.UseSqlServer(op, sql => sql.MigrationsAssembly(migrationsAssembly)))
+                .AddOperationalStore(options =>
+                {
+                    options.ConfigureDbContext = b => b.UseSqlServer(op, sql => sql.MigrationsAssembly(migrationsAssembly));
+                    options.EnableTokenCleanup = true;
+                    options.TokenCleanupInterval = 30;
+                }); 
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            InitializeDatabase(app);
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseIdentityServer();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in IdentityServerConfig.GetClients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in IdentityServerConfig.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in IdentityServerConfig.GetApis())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
+        }
+
+    }
+}
