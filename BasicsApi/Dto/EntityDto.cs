84325b9e599df2c41c48c07961dd3c0fb724ccdd
mengzujun <943885179@qq.com>
2019-11-14 18:56:29 +0800

    添加批量删除菜单

---
+++ BasicsApi/Dto/EntityDto.cs
@@ -0,0 +1,7 @@
+namespace BasicsApi.Dto
+{
+    public class EntityDto
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ BasicsApi/Dto/SelectDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicsApi.Dto
+{
+    public class SelectDto
+    {
+        public string title { get; set; }
+        public string label { get; set; }
+        public string value { get; set; }
+        public int key { get; set; }
+        public bool isChecked { get; set; }
+        public bool IsHave { get; set; }
+        public bool disabled { get; set; }
+        public List<SelectDto> children { get; set; }
+
+    }
+}
