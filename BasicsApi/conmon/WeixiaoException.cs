e2be47c7de7f9c932d14e0cb97a4c9bad84292d0
mengzujun <943885179@qq.com>
2019-11-19 08:47:23 +0800

    自定义异常提交

---
+++ BasicsApi/conmon/WeixiaoException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicsApi.conmon
+{
+    public class WeixiaoException : ApplicationException
+    {
+        public WeixiaoException() : base("测试异常")
+        {
+
+        }
+        public WeixiaoException(string message) : base(message)
+        {
+
+        }
+        public WeixiaoException(string message, Exception inner) : base(message, inner)
+        {
+
+        }
+    }
+}
