31622a6e45c0969ac4a32dc1d73297e184b7c6a8
mengzujun <943885179@qq.com>
2020-03-11 18:40:42 +0800

    feat：添加节点右键

--- BasicsApi/Controllers/FlowController.cs
+++ BasicsApi/Controllers/FlowController.cs
@@ -28,77 +28,70 @@ namespace BasicsApi.Controllers
             {
                 FlowData = new FlowData()
                 {
-                    /* Nodes = new List<FlowNode>()
-                     {
-                         new FlowNode() { Id = "node1", Label = "1", Size = new int[] { 80 }, Shape = "rect", Style = new Style() { Fill = "blue" } },
-                         new FlowNode() { Id = "node2", Label = "2", Size = new int[] { 80, 40 }, Shape = "ellipse" },
-                         new FlowNode() { Id = "node3", Label = "3", Size = new int[] { 80, 20, 40, 5 }, Shape = "triangle" },
-                         new FlowNode() { Id = "node4", Label = "4", Size = new int[] { 80 }, LinkPoints = new LinkPoints { } },
-                         new FlowNode() { Id = "node5", Label = "5", Shape = "star", Style = new Style() { Fill = "red" } },
-                         new FlowNode() { Id = "node7", Label = "this s", Shape = "modelRect", Description = "谈谈他", Size = new int[] { 40, 50 } },
-                         new FlowNode() { Id = "node6", Label = "this is image", Img = "https://localhost:5001/upload/1.jpg", Shape = "image", Size = new int[] { 40, 20 } }
-                     },
-                     Edges = new List<FlowEdge>
-                     {
-                         new FlowEdge()
-                         {
-                             Source = "node1",
-                             Target = "node2",
-                             Label = "我是描述",
-                             LabelCfg = new LabelCfgs
-                             {
-                                 Position = "end"
-                             }
-                         },
-                         new FlowEdge()
-                         {
-                             Source = "node1",
-                             Target = "node3",
-                             Label = "dda",
-                             Shape = "quadratic"
-                         },
-                         new FlowEdge()
-                         {
-                             Source = "node2",
-                             Target = "node5",
-                             Label = "dda"
-                         },
-                         new FlowEdge()
-                         {
-                             Source = "node2",
-                             Target = "node4",
-                             Label = "dda"
-                         }
-                     }*/
+                //      Nodes = new List<FlowNode>()
+                //      {
+                //          new FlowNode() { Id = "node1", Label = "1", Size = new int[] { 80 }, Shape = "rect", Style = new Style() { Fill = "blue" } },
+                //          new FlowNode() { Id = "node2", Label = "2", Size = new int[] { 80, 40 }, Shape = "ellipse" },
+                //          new FlowNode() { Id = "node3", Label = "3", Size = new int[] { 80, 20, 40, 5 }, Shape = "triangle" },
+                //          new FlowNode() { Id = "node4", Label = "4", Size = new int[] { 80 }, LinkPoints = new LinkPoints { } },
+                //          new FlowNode() { Id = "node5", Label = "5", Shape = "star", Style = new Style() { Fill = "red" } },
+                //          new FlowNode() { Id = "node7", Label = "this s", Shape = "modelRect", Description = "谈谈他", Size = new int[] { 40, 50 } },
+                //          new FlowNode() { Id = "node6", Label = "this is image", Img = "https://localhost:5001/upload/1.jpg", Shape = "image", Size = new int[] { 40, 20 } }
+                //      },
+                //      Edges = new List<FlowEdge>
+                //      {
+                //          new FlowEdge()
+                //          {
+                //              Source = "node1",
+                //              Target = "node2",
+                //              Label = "我是描述",
+                //              LabelCfg = new LabelCfgs
+                //              {
+                //                  Position = "end"
+                //              }
+                //          },
+                //          new FlowEdge()
+                //          {
+                //              Source = "node1",
+                //              Target = "node3",
+                //              Label = "dda",
+                //              Shape = "quadratic"
+                //          },
+                //          new FlowEdge()
+                //          {
+                //              Source = "node2",
+                //              Target = "node5",
+                //              Label = "dda"
+                //          },
+                //          new FlowEdge()
+                //          {
+                //              Source = "node2",
+                //              Target = "node4",
+                //              Label = "dda"
+                //          }
+                //      }
                 },
                 FlowGraph = new FlowGraph()
                 {
                     // DefaultNode=new LabelCfgs { LinkPoints=new LinkPoints { } },
-                    Layout = new Layout
-                    {
-                    },
+                    //layout会让node节点有所偏移，需要进一步解决
+                    // Layout = new Layout
+                    // {
+                    // },
                     Modes = new Mode
                     {
                         // Default=new string[] { "drag-canvas", "drag-node" },
                         Default = new List<object>() {
-                            new DragCanvas() ,
-                            new DragNode(),
-                            new Tooltip(),
-                            new ClickSelect(),
-                            new EdgeTooltip(),
-                            new ActivateRelations(),
-                            new BrushSelect(),
+                           new DragCanvas() ,
+                           new DragNode(),
+                           new Tooltip(),
                         }
                     },
                     NodeStateStyles = new StateStyle
                     {
                         Hover = new Style
                         {
-                            Fill = "#d3adf7"
-                        },
-                        Running = new Style
-                        {
-                            Stroke = "steelblue"
+                            Fill = "red"
                         }
                     }
                 },
--- weixiaoAdmin/src/app/routes/dashboard/dashboard.component.ts
+++ weixiaoAdmin/src/app/routes/dashboard/dashboard.component.ts
@@ -7,127 +7,186 @@ import { HttpBasicService } from '@shared/utils/http-basic.service';
 import { BasicService } from 'src/app/service/basic.service';
 import { NzMessageService } from 'ng-zorro-antd';
 import { ITEM_TYPE } from '@antv/g6/lib/types';
+import * as insertCss from 'insert-css';
+import { log } from 'util';
 @Component({
-    selector: 'app-dashboard',
-    templateUrl: './dashboard.component.html',
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
 })
 export class DashboardComponent implements OnInit {
-    constructor(private http: HttpBasicService, private basic: BasicService, private msg: NzMessageService) { }
-    schema: SFSchema = {
-        properties: {
-            name: {
-                "type": "string"
-            },
-            password: {
-                "type": "string",
-                "ui": {
-                    "type": "password"
-                }
-            }
+  constructor(private http: HttpBasicService, private basic: BasicService, private msg: NzMessageService) { }
+  schema: SFSchema = {
+    properties: {
+      name: {
+        "type": "string"
+      },
+      password: {
+        "type": "string",
+        "ui": {
+          "type": "password"
+        }
+      }
+    },
+    "required": ["name", "password"]
+  }
+  graph: Graph;
+  type: ITEM_TYPE = "node";
+  thisNodeId = "";
+  nodeIndex = 0;
+  async  ngOnInit(): Promise<void> {
+    await this.insertCSS();
+    const conextMenuContainer = document.createElement('ul');
+    conextMenuContainer.id = 'contextMenu';
+    const firstLi = document.createElement('li');
+    firstLi.innerText = '开始/结束';
+    firstLi.onclick = () => { console.log(this.graph); this.graph.updateItem("node-1", { shape: "ellipse" }); this.graph.refreshItem("node-1"); };
+    conextMenuContainer.appendChild(firstLi);
+
+    const lastLi = document.createElement('li');
+    lastLi.innerText = 'Option 2';
+    conextMenuContainer.appendChild(lastLi);
+    document.getElementById('mountNode').appendChild(conextMenuContainer);
+    this.http.get(this.basic.ApiUrl + "Flow/test").subscribe(res => {
+      // tslint:disable-next-line: no-eval
+      res.flowGraph.width = eval(res.flowGraph.width);
+      // tslint:disable-next-line: no-eval
+      res.flowGraph.height = eval(res.flowGraph.height);
+      res.flowGraph.modes.default.forEach(mode => {
+        if (mode.type === "tooltip") {
+          // tslint:disable-next-line: no-eval
+          mode.formatText = eval(mode.formatText);
+        }
+      });
+      res.flowGraph.modes.addFlow = ['addFlow', 'click-select'];
+      G6.registerBehavior('addFlow', {
+        getEvents() {
+          return {
+            'canvas:click': 'onClickCanvas',// 点击界面生成节点
+            'node:click': 'onClickNode',// 点击节点开始生成线路
+            'mousemove': 'onMousemove',// 移动时候绘画线路
+            'edge:click': 'onEdgeClick',// 中途停止时候删除线路
+            'node:contextmenu': 'nodeContextmenu',//右键节点
+            'node:mouseleave': 'nodeMouseleave',
+          };
+        }, onClickCanvas(ev: any) {
+          if (this.nodeIndex === undefined) {
+            this.nodeIndex = 1;
+          }
+          this.graph.addItem('node', {
+            x: ev.canvasX,
+            y: ev.canvasY,
+            shape:'rect',
+            id: `node-` + this.nodeIndex,
+            label: `node-` + this.nodeIndex,
+            size: 60
+          });
+          this.nodeIndex++;
+        }, onClickNode(ev: any) {
+          if (this.addingEdge && this.edge) {
+            this.graph.updateItem(this.edge, {
+              target: ev.item.getModel().id,
+            });
+            this.edge = null;
+            this.addingEdge = false;
+          } else {
+            this.edge = this.graph.addItem('edge', {
+              type: "polyline",
+              source: ev.item.getModel().id,
+              target: { x: ev.x, y: ev.y },
+              style: {
+                //fill: `yellow`,
+                //stroke: `red`,
+                lineWidth: 3
+              }
+            });
+            this.addingEdge = true;
+          }
         },
-        "required": ["name", "password"]
-    }
-    graph: Graph;
-    type: ITEM_TYPE = "node";
-    nodeIndex = 0;
-    async  ngOnInit(): Promise<void> {
-        this.http.get(this.basic.ApiUrl + "Flow/test").subscribe(res => {
-            console.log(res.flowGraph);
-
-            // tslint:disable-next-line: no-eval
-            res.flowGraph.width = eval(res.flowGraph.width);
-            // tslint:disable-next-line: no-eval
-            res.flowGraph.height = eval(res.flowGraph.height);
-            res.flowGraph.modes.default.forEach(mode => {
-                if (mode.type === "tooltip") {
-                    // tslint:disable-next-line: no-eval
-                    mode.formatText = eval(mode.formatText);
-                }
+        onMousemove(ev: any) {
+          if (this.addingEdge && this.edge) {
+            this.graph.updateItem(this.edge, {
+              target: { x: ev.x, y: ev.y },
             });
-            res.flowGraph.modes.addFlow = ['addFlow', 'click-select'];
-            G6.registerBehavior('addFlow', {
-                getEvents() {
-                    return {
-                        'canvas:click': 'onClickCanvas',// 点击界面生成节点
-                        'node:click': 'onClickNode',// 点击节点开始生成线路
-                        'mousemove': 'onMousemove',// 移动时候绘画线路
-                        'edge:click': 'onEdgeClick',// 中途停止时候删除线路
-                    };
-                }, onClickCanvas(ev: any) {
-                    if (this.nodeIndex === undefined) {
-                        this.nodeIndex = 1;
-                    }
-                    this.graph.addItem('node', {
-                        x: ev.canvasX,
-                        y: ev.canvasY,
-                        id: `node-` + this.nodeIndex,
-                        label: `node-` + this.nodeIndex,
-                        size: 60
-                    });
-                    this.nodeIndex++;
-                }, onClickNode(ev: any) {
-                    if (this.addingEdge && this.edge) {
-                        this.graph.updateItem(this.edge, {
-                            target: ev.item.getModel().id,
-                        });
-                        this.edge = null;
-                        this.addingEdge = false;
-                    } else {
-                        this.edge = this.graph.addItem('edge', {
-                            type: "polyline",
-                            source: ev.item.getModel().id,
-                            target: { x: ev.x, y: ev.y },
-                            style: {
-                                //fill: `yellow`,
-                                //stroke: `red`,
-                                lineWidth: 3
-                            }
-                        });
-                        this.addingEdge = true;
-                    }
-                },
-                // The responsing function for mousemove defined in getEvents
-                onMousemove(ev: any) {
-                    if (this.addingEdge && this.edge) {
-                        // Update the end node to the current node the mouse clicks
-                        this.graph.updateItem(this.edge, {
-                            target: { x: ev.x, y: ev.y },
-                        });
-                    }
-                },
-                // The responsing function for edge:click defined in getEvents
-                onEdgeClick(ev: any) {
-                    const self = this;
-                    const currentEdge = ev.item;
-                    if (self.addingEdge && self.edge === currentEdge) {
-                        self.graph.removeItem(self.edge);
-                        self.edge = null;
-                        self.addingEdge = false;
-                    }
-                },
-            });
-            this.graph = new G6.Graph(res.flowGraph);
-
-            this.graph.data(res.flowData);
-            res.ons.forEach((onFun: any) => {
-                // tslint:disable-next-line: function-constructor
-                this.graph.on(onFun.funName, new Function(onFun.funParameter, onFun.funBody));
-            });
-            this.graph.render();
-        })
-    }
-    async save() {
-        this.msg.info(JSON.stringify(this.graph.save()));
-    }
-    async add(type: string) {
-        this.graph.add(this.type, { shape: type, id: this.type + this.nodeIndex, label: type + this.nodeIndex, x: 100, y: 100 });
-        this.nodeIndex++;
-        this.graph.refresh();
-    }
-    async addFlow() {
-        this.graph.setMode("addFlow");
-    }
-    async default() {
-        this.graph.setMode("default");
-    }
+          }
+        },
+        onEdgeClick(ev: any) {
+          const self = this;
+          const currentEdge = ev.item;
+          if (self.addingEdge && self.edge === currentEdge) {
+            self.graph.removeItem(self.edge);
+            self.edge = null;
+            self.addingEdge = false;
+          }
+        },
+        nodeContextmenu(ev: any) {
+          console.log(ev);
+          this.thisNodeId = ev.item.getModel().id;
+          ev.preventDefault();
+          ev.stopPropagation();
+          conextMenuContainer.style.left = `${ev.clientX}px`;
+          conextMenuContainer.style.top = `${ev.clientY}px`;
+        }, nodeMouseleave(ev: any) {
+          this.thisNodeId = "";
+          conextMenuContainer.style.left = '-150px';
+        }
+      });
+      this.graph = new G6.Graph(res.flowGraph);
+      this.graph.data(res.flowData);
+      res.ons.forEach((onFun: any) => {
+        // tslint:disable-next-line: function-constructor
+        this.graph.on(onFun.funName, new Function(onFun.funParameter, onFun.funBody));
+      });
+      this.graph.render();
+    })
+  }
+  async Test() {
+    alert(123);
+  }
+  async insertCSS() {
+    insertCss(`
+      /*.g6-tooltip {
+          border: 1px solid #e2e2e2;
+          border-radius: 4px;
+          font-size: 12px;
+          color: #545454;
+          background-color: rgba(255, 255, 255, 0.9);
+          padding: 10px 8px;
+          box-shadow: rgb(174, 174, 174) 0px 0px 10px;
+        }*/
+        #contextMenu {
+          position: absolute;
+          list-style-type: none;
+          padding: 10px 8px;
+          left: -150px;
+          background-color: rgba(255, 255, 255, 0.9);
+          border: 1px solid #e2e2e2;
+          border-radius: 4px;
+          font-size: 12px;
+          color: #545454;
+        }
+        #contextMenu li {
+          cursor: pointer;
+		      list-style-type:none;
+          list-style: none;
+          margin-left: 0px;
+        }
+        #contextMenu li:hover {
+          color: #aaa;
+        }
+      `);
+  }
+  async save() {
+    this.msg.info(JSON.stringify(this.graph.save()));
+  }
+  async add(type: string) {
+    this.graph.add(this.type, { shape: type, id: this.type + this.nodeIndex, label: type + this.nodeIndex, x: 100, y: 100 });
+    this.nodeIndex++;
+    this.graph.refresh();
+  }
+  async addFlow() {
+    this.graph.setMode("addFlow");
+  }
+  async default() {
+    this.graph.setMode("default");
+  }
 }
